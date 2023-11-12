fb70bd99f0425b700b892c1619c28ad8bcf8b394
noeld <noel.dardeau@edu.ece.fr>
2023-11-12 16:16:41 +0100

    création

---
+++ niveau.c
@@ -0,0 +1,5 @@
+//
+// Created by noeld on 12/11/2023.
+//
+
+#include "niveau.h"
---
+++ niveau.h
@@ -0,0 +1,8 @@
+//
+// Created by noeld on 12/11/2023.
+//
+
+#ifndef PROJETTG_NIVEAU_H
+#define PROJETTG_NIVEAU_H
+
+#endif //PROJETTG_NIVEAU_H
