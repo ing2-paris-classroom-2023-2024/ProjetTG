f29586013542ab957fe5585c71605b6b0e477115
Vavadubled <vadimdzierzbicki@gmail.com>
2023-11-24 22:05:19 +0100

    Ajout du début du DFS (ne marche pas + probleme de mémoire)

--- algo_remplissage.c
+++ algo_remplissage.c
@@ -11,37 +11,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct f_File {
-    int* elements;
-    int tete;
-    int queue;
-    int taille;
-} t_File;
 
-typedef struct {
-    int* sommets;
-    float taille;
-} t_sommets;
+
 
 // déscente de graphe
 
 
-void MAJ_sommets(int** matriceNiveaux, int nbSommet, int niveau,t_sommets sommets) {
-    int nbOperations = nb_operations();
-    int* num_operations = malloc(nbOperations * sizeof(int));
-    float* duree_operations = malloc(nbOperations * sizeof(float));
-    init_operations(nbOperations, num_operations, duree_operations);
+t_sommets MAJ_sommets(int** matriceNiveaux, int nbSommet, int niveau,int* num_operations, float* duree_operations, int nbOperations) {
+    t_sommets sommets;
+    t_File sommet;
     for (int i = 0; i < niveau; ++i) {
         for (int j = 0; j < nbSommet; ++j) {
             if (matriceNiveaux[i][j] != -1) {
                 for (int k = 0; k < nbOperations; ++k) {
                     if (matriceNiveaux[i][j] == num_operations[k]) {
                         sommets.sommets[j] =matriceNiveaux[i][j];
-                        sommets.taille = duree_operations[k];
+                        sommets.taille[j] = duree_operations[k];
+                        //sommet.elements = &sommets;
                     }
                 }
             }
         }
     }
+    return sommets;
 }
 
+void print_Maj_sommet(int niveau,int nbSommet,t_sommets sommets){
+    for (int i=0; i<niveau;i++){
+        for (int j=0; j< nbSommet;j++ ){
+            printf("\n%d %f", sommets.sommets[j], sommets.taille[j]);
+        }
+    }
+}
--- main.c
+++ main.c
@@ -3,14 +3,18 @@
 #include "Lecture_Fichier.h"
 #include "MatriceNiveau.h"
 #include "Exclusions.h"
+#include "algo_remplissage.h"
 
 int main(){
-
+    int nbOperations = nb_operations();
+    int* num_operations = malloc(nbOperations * sizeof(int));
+    float* duree_operations = malloc(nbOperations * sizeof(float));
+    init_operations(nbOperations, num_operations, duree_operations);
     struct Graph* graphe = initGraph();
     int nb_sommets_depart=0;
     int * sommetsDepart = TrouverSommetsDepart(graphe, &nb_sommets_depart);
     int** matriceNiveaux = malloc(graphe->nbSommet * sizeof(int*));
-
+    t_sommets sommets;
     int* niveau = CreerMatriceNiveaux(graphe, sommetsDepart, nb_sommets_depart, matriceNiveaux);
     printf("\nMatrice des niveaux :\n\n");
     printMatriceNiveaux(matriceNiveaux, graphe->nbSommet, *niveau);
@@ -21,18 +25,19 @@ int main(){
     //afficherPaires(exclusions);
     comparerExclusionsAvecMatriceNiveaux(graphe, &matriceNiveaux,niveau, graphe->nbSommet, exclusions);
     printf("\nMatrice des niveaux avec exclusions :\n\n");
-    printMatriceNiveaux(matriceNiveaux, graphe->nbSommet, *niveau);
+    printMatriceNiveaux(matriceNiveaux, graphe->nbSommet, *niveau);/*
+   printf("affichage des sommets");
+    sommets = MAJ_sommets(matriceNiveaux,graphe->nbSommet,*niveau,num_operations,duree_operations,nbOperations);
+    print_Maj_sommet(*niveau,graphe->nbSommet,sommets);*/
     //printf("\n%d",sont_incompatibles(1, 4, exclusions));
-    /*int nbOperations = nb_operations();
-    int* num_operations = malloc(nbOperations * sizeof(int));
-    float* duree_operations = malloc(nbOperations * sizeof(float));
-    init_operations(nbOperations, num_operations, duree_operations);
+
     printf("\n\nOperations :\n\n");
     //print_operations(nbOperations, num_operations, duree_operations);
 
     //free(incompatibilites);
     free(num_operations);
-    free(duree_operations);*/
+    free(duree_operations);
+
     free(sommetsDepart);
     free(graphe);
     free(matriceNiveaux);
